80a5ca6f5f1fa303db3110513c1f61224cf901fa
CyrilArtFX <ccrozat@artfx.fr>
2023-05-07 16:34:34 +0200

    [game] integrated stoneshard new map

--- SimpleEngineGames/Games/Stoneshard/ActorsComponents/StoneshardMap.cpp
+++ SimpleEngineGames/Games/Stoneshard/ActorsComponents/StoneshardMap.cpp
@@ -1,11 +1,31 @@
 #include "StoneshardMap.h"
 #include <Components/GridComponents/GridTileDrawRectangle.h>
+#include <Components/GridComponents/GridTileDrawSprite.h>
+#include <SimpleEngine/Assets/Assets.h>
 
 StoneshardMap::StoneshardMap(GridMap* gridMapP)
 {
 	gridComp = new GridComponent(this, gridMapP);
 	gridComp->setTileSize(Vector2{ 32.0f, 32.0f });
 
-	gridComp->setTileTraduction(0, new TileTraduction{ new GridTileDrawRectangle(Color(154, 205, 50, 255), Rectangle::unitRect), true });
-	gridComp->setTileTraduction(1, new TileTraduction{ new GridTileDrawRectangle(Color(34, 139, 34, 255), Rectangle::unitRect), true });
+	/*gridComp->setTileTraduction(0, new TileTraduction{new GridTileDrawRectangle(Color(154, 205, 50, 255), Rectangle::unitRect), true});
+	gridComp->setTileTraduction(1, new TileTraduction{ new GridTileDrawRectangle(Color(34, 139, 34, 255), Rectangle::unitRect), true });*/
+
+	gridComp->setTileTraduction(1, new TileTraduction{ new GridTileDrawSprite(Assets::getTexture("tile_tree_top")), true });
+	gridComp->setTileTraduction(2, new TileTraduction{ new GridTileDrawSprite(Assets::getTexture("tile_grass")), true });
+	gridComp->setTileTraduction(3, new TileTraduction{ new GridTileDrawSprite(Assets::getTexture("tile_big_tree_top_left")), true });
+	gridComp->setTileTraduction(4, new TileTraduction{ new GridTileDrawSprite(Assets::getTexture("tile_big_tree_top_right")), true });
+	gridComp->setTileTraduction(5, new TileTraduction{ new GridTileDrawSprite(Assets::getTexture("tile_tree_middle")), true });
+	gridComp->setTileTraduction(6, new TileTraduction{ new GridTileDrawSprite(Assets::getTexture("tile_bush")), true });
+	gridComp->setTileTraduction(7, new TileTraduction{ new GridTileDrawSprite(Assets::getTexture("tile_big_tree_middle_left")), true });
+	gridComp->setTileTraduction(8, new TileTraduction{ new GridTileDrawSprite(Assets::getTexture("tile_big_tree_middle_right")), true });
+	gridComp->setTileTraduction(9, new TileTraduction{ new GridTileDrawSprite(Assets::getTexture("tile_tree_bottom")), true });
+	gridComp->setTileTraduction(10, new TileTraduction{ new GridTileDrawSprite(Assets::getTexture("tile_small_tree")), true });
+	gridComp->setTileTraduction(11, new TileTraduction{ new GridTileDrawSprite(Assets::getTexture("tile_big_tree_bottom_left")), true });
+	gridComp->setTileTraduction(12, new TileTraduction{ new GridTileDrawSprite(Assets::getTexture("tile_big_tree_bottom_right")), true });
+
+	//  if anyone see this, yeah I know this is horrible, I will do a proper integration of tilesheet but I don't have time to do that now, it will be done this summer
+
+	
+	gridComp->setAstarTraduction(2, AstarTraduction{ true, 10, 14 });
 }
--- SimpleEngineGames/Games/Stoneshard/StoneshardScene.cpp
+++ SimpleEngineGames/Games/Stoneshard/StoneshardScene.cpp
@@ -15,9 +15,22 @@ void StoneshardScene::load(Game* game)
 	Assets::loadTexture(game->getRenderer(), "DirPathfinding.png", "dirPathfinding");
 	Assets::loadTexture(game->getRenderer(), "Player.png", "player");
 	Assets::loadGridMap("StoneshardMapV1.lua", "mapV1");
+	Assets::loadGridMap("StoneshardMapV2.lua", "mapV2");
+	Assets::loadTexture(game->getRenderer(), "Tiles/grass.png", "tile_grass");
+	Assets::loadTexture(game->getRenderer(), "Tiles/bush.png", "tile_bush");
+	Assets::loadTexture(game->getRenderer(), "Tiles/small_tree.png", "tile_small_tree");
+	Assets::loadTexture(game->getRenderer(), "Tiles/tree_bottom.png", "tile_tree_bottom");
+	Assets::loadTexture(game->getRenderer(), "Tiles/tree_middle.png", "tile_tree_middle");
+	Assets::loadTexture(game->getRenderer(), "Tiles/tree_top.png", "tile_tree_top");
+	Assets::loadTexture(game->getRenderer(), "Tiles/big_tree_bottom_left.png", "tile_big_tree_bottom_left");
+	Assets::loadTexture(game->getRenderer(), "Tiles/big_tree_bottom_right.png", "tile_big_tree_bottom_right");
+	Assets::loadTexture(game->getRenderer(), "Tiles/big_tree_middle_left.png", "tile_big_tree_middle_left");
+	Assets::loadTexture(game->getRenderer(), "Tiles/big_tree_middle_right.png", "tile_big_tree_middle_right");
+	Assets::loadTexture(game->getRenderer(), "Tiles/big_tree_top_left.png", "tile_big_tree_top_left");
+	Assets::loadTexture(game->getRenderer(), "Tiles/big_tree_top_right.png", "tile_big_tree_top_right");
 
 	auto manager = new StoneshardManager();
-	auto map = new StoneshardMap(Assets::getGridMap("mapV1"));
+	auto map = new StoneshardMap(Assets::getGridMap("mapV2"));
 
 	auto player_inputs = new StoneshardPlayerInputs(manager, map);
 }
